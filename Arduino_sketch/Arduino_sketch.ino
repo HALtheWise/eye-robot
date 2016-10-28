73eba70f246255785da74daf2080444b3c4d29c9
arpanrau <arpan.rau@gmail.com>
2016-10-27 23:20:41 -0400

    finished arduino code?

--- Arduino_sketch/Arduino_sketch.ino
+++ Arduino_sketch/Arduino_sketch.ino
@@ -1,9 +1,14 @@
 //Reads Serial and controls Eye-Robot
 #include <Servo.h>
-const int leftmotor = 3;//left motor on pin 3 PWM
+const int leftmotorpin = 3;//left motor pin 3 PWM
 const int leftmotordir = 4;// left motor direction on pin 4
-const int rightmotor = 5;//right motor on pin 5 PWM
-const int rightmotordir = 6;//right motor on pin 6 PWMz
+const int liftmotordir2 = 2; //left motor direction 2 on pint 2
+const int rightmotorpin = 5;//right motor on pin 5 PWM
+const int rightmotordir = 6;//right motor on pin 6 PWM
+const int rightmotordir2 = 7;//right motor 2 on pin 6 PWM
+const int leftservopin = 9;//leftservopin on pin 9
+const int rightservopin = 10;//rightservopin on pin 10
+
 
 
 float fwdspeed = 0; 
@@ -11,19 +16,28 @@ float turnspeed = 0;
 float pan = 0;
 float tilt = 0;
 
+Servo leftservo;
+Servo rightservo;
+
 void setup()
 {
   Serial.begin(115200);//Begin serial at 115000 baud
   leftservo.attach(9);//attaches left servo to pin 9
   rightservo.attach(10);//attaches right servo to pin 10
-  pinMode(rightmotor, OUTPUT);
-  pinMode(leftmotor, OUTPUT);
+  pinMode(rightmotorpin, OUTPUT);
+  pinMode(leftmotorpin, OUTPUT);
+  pinMode(rightmotordir, OUTPUT);
+  pinMode(leftmotordir, OUTPUT);
+  pinMode(rightmotordir2, OUTPUT);
+  pinMode(leftmotordir2, OUTPUT);
+  
 }
 
 void loop()
 {
+  
   //if there's any serial avaliable , read it
-  while(Serial.available() > 0){
+  while(Serial.available() > 1){
     //look for each valid float in the serial stream
     fwdspeed = Serial.parseFloat();
     turnspeed = Serial.parseFloat();
@@ -32,28 +46,60 @@ void loop()
   }
   //on newline, control hardware
   if(Serial.read() == '\n') {  
-  motorcontroller(fwdspeed,turnspeed);
-  servocontroller(pan, tilt);
-  delay(15);
+    motorcontroller(fwdspeed,turnspeed);
+    servocontroller(pan, tilt);
   }
+  //Clear the serial buffer
+  while (Serial.available()>0){
+    Serial.read();
+  }
+  delay(10);
 }
 
 void motorcontroller(float fwd, float turn){
   //given a forward speed and turn speed controls motors
-}
+  float leftmotor = fwd + turn
+  float rightmotor = fwd - turn
+  normalizepowers(&leftmotor, &rightmotor, 1.0)
+  if leftmotor <= 0:
+    digitalWrite(leftmotordir, HIGH);
+    digitalWrite(leftmotordir2, LOW);
+    analogWrite(leftmotorpin, leftmotor*255);
+  if leftmotor > 0:
+    digitalWrite(leftmotordir, LOW);
+    digitalWrite(leftmotordir2, HIGH);
+    analogWrite(leftmotorpin, leftmotor*255);
+  if rightmotor <= 0:
+    digitalWrite(rightmotordir, HIGH);
+    digitalWrite(rightmotordir2, LOW);
+    analogWrite(rightmotorpin, rightmotor*255);
+  if rightmotor > 0:
+    digitalWrite(rightmotordir, LOW);
+    digitalWrite(rightmotordir2, HIGH);
+    analogWrite(rightmotorpin, rightmotor*255);
+  break
 
 void servocontroller(float panpos, float tiltpos){
   // given a pan and tilt value controls the differential
-  int leftpos
-  int rightpos
-  leftpos = pan - tilt
-  rightpos = pan + tilt
+  int leftpos;
+  int rightpos;
+  leftpos = pan - tilt;
+  rightpos = pan + tilt;
   //converts from -90 to 90 to 0 to 180
-  leftpos = leftpos + 90
-  rightpos = rightpos + 90
+  leftpos += 90;
+  rightpos += 90;
   //write values to servos
   leftservo.write(leftpos);
   rightservo.write(rightpos);
+}  
+
+
+void normalizePowers(float *left, float *right, float limit){
+	int maxabs = max(abs(*left), abs(*right));
+	if(maxabs > limit)
+	{
+	    *left = (*left * limit) / maxabs;
+	    *right = (*right * limit) / maxabs;
+	}
 }
-  
- 
+
