477d05e4cbb0f45e64d93e027eb7a4cdd2260458
Eric Miller <eric@legoaces.org>
2016-11-11 00:26:58 -0500

    Updated pin numbers for motor control

--- Arduino_sketch/Arduino_sketch.ino
+++ Arduino_sketch/Arduino_sketch.ino
@@ -1,10 +1,10 @@
 //Reads Serial and controls Eye-Robot
 #include <Servo.h>
-const int leftmotorpin = 9;//left motor pin 3 PWM
+const int leftmotorpin = 3;//left motor pin 3 PWM
 const int leftmotordir = 2;// left motor direction on pin 4
-const int leftmotordir2 = 3; //left motor direction 2 on pint 2
-const int rightmotorpin = 5;//right motor on pin 5 PWM
-const int rightmotordir = 6;//right motor on pin 6 PWM
+const int leftmotordir2 = 4; //left motor direction 2 on pint 2
+const int rightmotorpin = 6;//right motor on pin 5 PWM
+const int rightmotordir = 5;//right motor on pin 6 PWM
 const int rightmotordir2 = 7;//right motor 2 on pin 6 PWM
 const int leftservopin = 9;//leftservopin on pin 9
 const int rightservopin = 10;//rightservopin on pin 10
@@ -70,22 +70,21 @@ void motorcontroller(float fwd, float turn){
   if (leftmotor <= 0){
     digitalWrite(leftmotordir, HIGH);
     digitalWrite(leftmotordir2, LOW);
-    analogWrite(leftmotorpin, leftmotor*255);
-  }
-  if (leftmotor > 0){
+    analogWrite(leftmotorpin, abs(leftmotor*255));
+  }else{
     digitalWrite(leftmotordir, LOW);
     digitalWrite(leftmotordir2, HIGH);
-    analogWrite(leftmotorpin, leftmotor*255);
+    analogWrite(leftmotorpin, abs(leftmotor*255));
   }
+
   if (rightmotor <= 0){
     digitalWrite(rightmotordir, HIGH);
     digitalWrite(rightmotordir2, LOW);
-    analogWrite(rightmotorpin, rightmotor*255);
-  }
-  if (rightmotor > 0){
+    analogWrite(rightmotorpin, abs(rightmotor*255));
+  }else{
     digitalWrite(rightmotordir, LOW);
     digitalWrite(rightmotordir2, HIGH);
-    analogWrite(rightmotorpin, rightmotor*255);
+    analogWrite(rightmotorpin, abs(rightmotor*255));
   }
 }
 
